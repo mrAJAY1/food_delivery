23022d45ac31467b6b88565af2b7882ab32837e3
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-09-03 22:33:53 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
