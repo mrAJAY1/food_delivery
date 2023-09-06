dfda0be43fc9b77ce2a9754c215d836c6cbdecfd
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-09-07 02:46:13 +0530

    basic state usage

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import resList from "../utils/mockData";
 import ResCard from "./ResCard";
 
 const Body = () => {
+  const [restaurantList, setRestaurantList] = useState(resList);
   return (
     <div className="res-body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = restaurantList.filter(
+              res => res.data.avgRating >= 4
+            );
+            filteredList.sort((a, b) => b.data.avgRating - a.data.avgRating);
+            setRestaurantList(filteredList);
+          }}>
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map(restaurant => {
+        {restaurantList.map(restaurant => {
           return <ResCard key={restaurant.data.id} resData={restaurant} />;
         })}
       </div>
