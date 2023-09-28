8016b826af5bc88e803336d7e49860d3ed8c5249
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-09-28 06:44:24 +0530

    useffect bug fix

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -3,10 +3,9 @@ import { MENU_API } from "./constants";
 
 const useRestaurantMenu = resId => {
   const [resInfo, setResInfo] = useState(null);
-
   useEffect(() => {
     fetchMenu();
-  });
+  },[]);
 
   const fetchMenu = async () => {
     const data = await fetch(MENU_API + resId);
